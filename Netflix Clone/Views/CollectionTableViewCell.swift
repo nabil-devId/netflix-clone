9565404cb1059e75c3eef14a9410a5447b8a8706
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2024-05-09 23:56:36 +0700

    Init commit - setup collection view

---
+++ Netflix Clone/Views/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Ahmad Nabil on 09/05/24.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
