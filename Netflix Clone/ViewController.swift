ded9fe101ff34cdb6aa00f58c67287f9bfe9bae0
ap-ahmad-nabil <ahmad.nabil@astrapay.com>
2024-05-09 22:46:50 +0700

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Ahmad Nabil on 09/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
